48fd23206cb2b23b77b2dea46febe2c24a2edc44
Bandari Ramcharan <118244554+Ramcharan05@users.noreply.github.com>
2025-05-23 00:25:00 +0530

    Update TodoRepository.java

--- todo/src/main/java/com/example/todo/repository/TodoRepository.java
+++ todo/src/main/java/com/example/todo/repository/TodoRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    List<Todo> findByCompletedFalse(); // Add this method
+    List<Todo> findByCompletedFalse();
 }
